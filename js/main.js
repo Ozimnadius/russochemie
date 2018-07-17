ed76a49a796dfad568b57f9a5192e18a91648658
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-07-17 16:15:24 +0300

    Главная статичная верстка, алÑv0.1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    /*SWIPER*/
+    var mainSwiper = new Swiper('.main__swiper', {
+        direction: 'horizontal',
+        slidesPerView: 4,
+        spaceBetween: 65,
+        navigation: {
+            nextEl: '.main__next',
+            prevEl: '.main__prev',
+        },
+    });
+    /*END SWIPER*/
+
+
+});
--- smartgrid.js
+++ smartgrid.js
@@ -1,35 +1,34 @@
 const smartgrid = require('smart-grid');
 
-smartgrid('./scss/layout', {
+smartgrid('./css/layout', {
     mobileFirst: false,
     columns: 24,
     offset: "30px",
     outputStyle: "scss",
     container: {
-        maxWidth: "1200px",
-        fields: "30px",
-        containerWidth: "1200px"
+        maxWidth: "1750px",
+        fields: "15px",
     },
     breakPoints: {
+        xlg: {
+            width: "1749.99px",
+            fields: "15px",
+        },
         lg: {
             width: "1199.99px",
             fields: "15px",
-            containerWidth: "990px"
         },
         md: {
             width: "991.99px",
             fields: "15px",
-            containerWidth: "750px"
         },
         sm: {
             width: "767.99px",
             fields: "15px",
-            containerWidth: "570px"
         },
         xs: {
             width: "575.99px",
             fields: "15px",
-            containerWidth: "100%"
         }
     },
 });
